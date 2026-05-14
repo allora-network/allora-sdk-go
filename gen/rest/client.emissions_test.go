393baca1b997211a297f80fb79b25dd783519bbd
spooktheducks <spooktheducks@protonmail.com>
2026-05-14 07:32:14 -0500

    test: cover v10 submission window routes
    Addresses PR review: add REST route tests for the new open reputer and worker submission-window endpoints.

--- gen/rest/client.emissions_test.go
+++ gen/rest/client.emissions_test.go
@@ -28,3 +28,41 @@ func TestEmissionsRESTClientUsesV10LatestInputInferenceRoute(t *testing.T) {
 	require.NoError(t, err)
 	require.Equal(t, "/emissions/v10/topics/7/workers/allo1worker/latest_input_inference", gotPath)
 }
+
+func TestEmissionsRESTClientUsesV10OpenReputerSubmissionWindowsRoute(t *testing.T) {
+	gotPath := captureEmissionsRESTPath(t, func(ctx context.Context, client *EmissionsRESTClient) error {
+		_, err := client.GetOpenReputerSubmissionWindows(ctx, &emissionstypes.GetOpenReputerSubmissionWindowsRequest{
+			TopicId: 42,
+		})
+		return err
+	})
+
+	require.Equal(t, "/emissions/v10/open_reputer_submission_windows/42", gotPath)
+}
+
+func TestEmissionsRESTClientUsesV10OpenWorkerSubmissionWindowsRoute(t *testing.T) {
+	gotPath := captureEmissionsRESTPath(t, func(ctx context.Context, client *EmissionsRESTClient) error {
+		_, err := client.GetOpenWorkerSubmissionWindows(ctx, &emissionstypes.GetOpenWorkerSubmissionWindowsRequest{
+			TopicId: 42,
+		})
+		return err
+	})
+
+	require.Equal(t, "/emissions/v10/open_worker_submission_windows/42", gotPath)
+}
+
+func captureEmissionsRESTPath(t *testing.T, call func(context.Context, *EmissionsRESTClient) error) string {
+	t.Helper()
+
+	var gotPath string
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		gotPath = r.URL.Path
+		_, _ = w.Write([]byte("{}"))
+	}))
+	defer server.Close()
+
+	client := NewEmissionsRESTClient(NewRESTClientCore(server.URL, zerolog.Nop()), zerolog.Nop())
+	require.NoError(t, call(context.Background(), client))
+
+	return gotPath
+}
